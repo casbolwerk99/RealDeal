3775b238ad4f756f6e947695a187d67958f8b11e
SamAlhabash <61803442+SamAlhabash@users.noreply.github.com>
2024-12-15 21:54:39 +0100

    feat: 🗃️ add postgres integration with users

---
+++ src/main/java/com/marketplace/realdeal/users/UserController.java
@@ -0,0 +1,30 @@
+package com.marketplace.realdeal.users;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
