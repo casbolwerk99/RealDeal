242b1dc8c2feaac0fa09f5db1279be940176d8e9
casbolwerk99 <casbolwerk99@gmail.com>
2024-12-15 17:33:00 +0100

    feat: initial commit

---
+++ src/main/java/com/marketplace/realdeal/RealdealApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.realdeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealdealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealdealApplication.class, args);
+	}
+
+}
