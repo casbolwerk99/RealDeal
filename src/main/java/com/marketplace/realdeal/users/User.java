945321beec9a4f449d7a10f10fa92d0d2b150474
casbolwerk99 <casbolwerk99@gmail.com>
2024-12-16 22:39:02 +0100

    feat: lombok getters and setters

--- src/main/java/com/marketplace/realdeal/users/User.java
+++ src/main/java/com/marketplace/realdeal/users/User.java
@@ -5,8 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "users")
 public class User {
     @Id
@@ -14,28 +18,4 @@ public class User {
     private Long id;
     private String name;
     private String email;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
